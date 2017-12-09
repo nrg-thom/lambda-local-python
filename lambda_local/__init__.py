0210676e4473c847f2b3f06fcb4e639e3dad00d8
Will Wenhua Yang <will.whyang@gmail.com>
2017-12-09 18:02:41 +1100

    add an simple example

--- lambda_local/__init__.py
+++ lambda_local/__init__.py
@@ -10,11 +10,13 @@ class LambdaContext(object):
     def __init__(self, function_name, region):
         self.function_name = function_name
         self.function_version = '$LATEST'
-        self.invoked_function_arn = 'arn:aws:lambda:{}:123456789012:function:{}'.format(region, function_name)
+        self.invoked_function_arn = 'arn:aws:lambda:{}:123456789012:function:{}'.format(
+            region, function_name)
         self.memory_limit_in_mb = '128'
         self.aws_request_id = str(uuid.uuid4())
         self.log_group_name = '/aws/lambda/{}'.format(function_name)
-        self.log_stream_name = '{}/[$LATEST]{}'.format(time.strftime('%Y/%m/%d', time.gmtime()), uuid.uuid4().hex)
+        self.log_stream_name = '{}/[$LATEST]{}'.format(time.strftime(
+            '%Y/%m/%d', time.gmtime()), uuid.uuid4().hex)
         self.identity = None
         self.client_context = None
 
@@ -22,7 +24,7 @@ class LambdaContext(object):
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--function', type=str, required=True,
-        help='Full path of the function, include directory, module, and function name')
+        help='Full path of the function, include module path and function name')
     parser.add_argument('-l', '--library', type=str, required=False, help='extra library path')
     parser.add_argument('-t', '--timeout', type=int, required=False, default=300,
         help='Lambda timeout in seconds, default 300')
@@ -31,24 +33,23 @@ def parse_args():
     parser.add_argument('-e', '--event', type=str, required=False, default='{}',
         help='JSON formated event, use file version if -E is specified')
     parser.add_argument('-E', '--event_file', type=str, required=False,
-        help='File path contains JSON formatted event, use file version if this option is specified')
+        help='File path contains JSON formatted event, use file version if -E is specified')
     return parser.parse_args()
 
 
 def invoke_function():
     args = parse_args()
 
-    module_path = args.function
-
-    if '.' not in module_path:
-        raise Exception('Incorrect function: "{}", e.g. path.to.module.function_name'.format(module_path))
+    if '.' not in args.function:
+        raise Exception(
+            'Incorrect function: "{}", e.g. path.to.module.function_name'.format(args.function))
 
     sys.path.append('.')
 
     if args.library:
         sys.path.append(args.library)
 
-    module_name, function_name = module_path.rsplit('.', 1)
+    module_name, function_name = args.function.rsplit('.', 1)
 
     module = importlib.import_module('{}'.format(module_name))
     if args.event_file:
@@ -57,7 +58,3 @@ def invoke_function():
         event = json.loads(args.event)
 
     getattr(module, function_name)(event, LambdaContext(function_name, args.region))
-
-
-if __name__ == '__main__':
-    invoke_function()
